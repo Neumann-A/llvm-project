0213e80f1528f11383aa1c841597e14294cbcdc5
Alexander Neumann <30894796+Neumann-A@users.noreply.github.com>
2023-11-29 10:46:41 +0100

    Revert "Reland "[Windows SEH]: HARDWARE EXCEPTION HANDLING (MSVC -EHa) - Part 2""
    This reverts commit 0efe111365ae176671e01252d24028047d807a84.

--- llvm/lib/CodeGen/BranchFolding.cpp
+++ llvm/lib/CodeGen/BranchFolding.cpp
@@ -1209,7 +1209,7 @@ bool BranchFolder::OptimizeBranches(MachineFunction &MF) {
     MadeChange |= OptimizeBlock(&MBB);
 
     // If it is dead, remove it.
-    if (MBB.pred_empty() && !MBB.isMachineBlockAddressTaken()) {
+    if (MBB.pred_empty()) {
       RemoveDeadBlock(&MBB);
       MadeChange = true;
       ++NumDeadBlocks;
